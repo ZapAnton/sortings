facfc77e62892f61f2d5675e51e8f9135bd3d83a
ZapAnton <wc3ft2@gmail.com>
2018-03-16 00:47:33 +0300

    Added bubble_sort function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn bubble_sort(array: &[i32]) {
+    println!("{:?}", array);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let initial_array = [1, 100, 8, 9, -87, 33, 68, 1025, 999, 43];
+
+    bubble_sort(&initial_array);
 }
