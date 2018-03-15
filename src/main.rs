1a98e0535ae8ffb5f344c0a1769da660817c9ed2
ZapAnton <wc3ft2@gmail.com>
2018-03-16 01:02:29 +0300

    Implemented bubble sort function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
-fn bubble_sort(array: &[i32]) {
-    println!("{:?}", array);
+fn bubble_sort(array: &mut [i32]) {
+    for i in 0..array.len() {
+        for j in i..array.len() {
+            if array[i] > array[j] {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
 }
 
 fn main() {
-    let initial_array = [1, 100, 8, 9, -87, 33, 68, 1025, 999, 43];
+    let mut initial_array = [1, 100, 8, 9, -87, 33, 68, 1, 1025, 999, 43];
 
-    bubble_sort(&initial_array);
+    bubble_sort(&mut initial_array);
+
+    println!("{:?}", initial_array);
 }
