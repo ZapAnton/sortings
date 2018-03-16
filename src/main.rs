a7833cca5a2cbf851fff8efa51db09e698b515c7
ZapAnton <wc3ft2@gmail.com>
2018-03-16 10:50:47 +0300

    Added closure parameter to bubble_sort function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-fn bubble_sort(array: &mut [i32]) {
+fn bubble_sort<F>(array: &mut [i32], compare_closure: F)
+where
+    F: Fn(i32, i32) -> bool,
+{
     for i in 0..array.len() {
         for j in i..array.len() {
-            if array[i] > array[j] {
+            if compare_closure(array[i], array[j]) {
                 let temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -13,7 +16,9 @@ fn bubble_sort(array: &mut [i32]) {
 fn main() {
     let mut initial_array = [1, 100, 8, 9, -87, 33, 68, 1, 1025, 999, 43];
 
-    bubble_sort(&mut initial_array);
+    let greater_closure = |x, y| x > y;
+
+    bubble_sort(&mut initial_array, greater_closure);
 
     println!("{:?}", initial_array);
 }
