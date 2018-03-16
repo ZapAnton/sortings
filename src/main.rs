1ceddfe42277d140d81c0c23c8183fca3f184fe9
ZapAnton <wc3ft2@gmail.com>
2018-03-16 12:38:12 +0300

    Implemented choose_comparison_closure function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 extern crate rand;
 
-fn bubble_sort<F>(array: &mut [i32], compare_closure: F)
-where
-    F: Fn(i32, i32) -> bool,
-{
+fn bubble_sort(array: &mut [i32], comparison_closure: &Box<Fn(i32, i32) -> bool>) {
     for i in 0..array.len() {
         for j in i..array.len() {
-            if compare_closure(array[i], array[j]) {
+            if comparison_closure(array[i], array[j]) {
                 let temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -47,6 +44,39 @@ fn generate_random_vector(items_count: i32) -> Vec<i32> {
         .collect()
 }
 
+fn choose_comparison_closure() -> Box<Fn(i32, i32) -> bool> {
+    use std::io::{stdin, stdout, Write};
+
+    print!(
+        "\nChoose comparison method:\n\
+         1. > (Greater than)\n\
+         2. < (Less than)\n\
+         3. >= (Greater than or equals)\n\
+         4. <= (Less than or equals)\n> "
+    );
+
+    let _ = stdout().flush();
+
+    let mut input_string = String::new();
+
+    stdin()
+        .read_line(&mut input_string)
+        .expect("Wrong string entered!");
+
+    let chosen_option: i32 = input_string.trim().parse().unwrap();
+
+    match chosen_option {
+        1 => Box::new(|x, y| x > y),
+        2 => Box::new(|x, y| x < y),
+        3 => Box::new(|x, y| x >= y),
+        4 => Box::new(|x, y| x <= y),
+        _ => {
+            println!("Wrong option. Choosing default method: > (Greater than)");
+            Box::new(|x, y| x > y)
+        }
+    }
+}
+
 fn main() {
     let items_count = get_user_items_count();
 
@@ -56,9 +86,9 @@ fn main() {
 
     let mut initial_vec = generate_random_vector(items_count);
 
-    let greater_closure = |x, y| x > y;
+    let comparison_closure = choose_comparison_closure();
 
-    bubble_sort(&mut initial_vec, greater_closure);
+    bubble_sort(&mut initial_vec, &comparison_closure);
 
     println!("Sorted items: {:?}", initial_vec);
 }
